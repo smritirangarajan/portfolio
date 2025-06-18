2d156ed48839030096da1d070bce38a256ce8724
smritirangarajan <smriti_rangarajan@berkeley.edu>
2025-06-18 11:14:28 -0700

    created data folder to better organize the templated data

--- app/__init__.py
+++ app/__init__.py
@@ -2,26 +2,43 @@ import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
+# Import page data
+from app.data.work_experiences import work_experiences
+from app.data.education import educations
+from app.data.hobbies import hobbies
+
 load_dotenv()
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html', title="Smriti Rangaragajan", url=os.getenv("URL"))
+    return render_template("index.html",
+                           title="Smriti Rangaragajan",
+                           url=os.getenv("URL"))
 
-@app.route('/work')
+# ---------- Work ----------
+@app.route("/work")
 def work_page():
-    return render_template('work.html', title="Work Experience")
+    return render_template("work.html",
+                           title="Work Experience",
+                           work_experiences=work_experiences)
 
-@app.route('/education')
+# ---------- Education ----------
+@app.route("/education")
 def education_page():
-    return render_template('education.html', title="Education")
+    return render_template("education.html",
+                           title="Education",
+                           educations=educations)
 
-@app.route('/hobbies')
+# ---------- Hobbies ----------
+@app.route("/hobbies")
 def hobbies_page():
-    return render_template('hobbies.html', title="Hobbies")
+    return render_template("hobbies.html",
+                           title="Hobbies",
+                           hobbies=hobbies)
 
-@app.route('/map')
+# ---------- Map ----------
+@app.route("/map")
 def map_page():
-    return render_template('map.html', title="Countries I've Visited")
+    return render_template("map.html",
+                           title="Countries I've Visited")
