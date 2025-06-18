ce89dcd7597458a07af9ca79652fa40cdf7121dd
smritirangarajan <smriti_rangarajan@berkeley.edu>
2025-06-18 10:20:16 -0700

    feat: add routes for work, education, hobbies, and map pages and created hobbies page (#9)

--- app/__init__.py
+++ app/__init__.py
@@ -9,3 +9,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+@app.route("/work")
+def work_page():
+    return render_template("work.html", title="Work Experience")
+
+@app.route("/education")
+def education_page():
+    return render_template("education.html", title="Education")
+
+@app.route("/hobbies")
+def hobbies_page():
+    return render_template("hobbies.html", title="Hobbies")
+
+@app.route("/map")
+def map_page():
+    return render_template("map.html", title="My Map")
