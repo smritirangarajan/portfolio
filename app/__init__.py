bcd8de4afe71a9aa995eace818edf39996653656
smritirangarajan <smriti_rangarajan@berkeley.edu>
2025-07-12 15:39:19 -0700

    adding timeline code

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,43 @@
 import os
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from peewee import * 
+import datetime
+from playhouse.shortcuts import model_to_dict
+
+
 
 # Import page data
 from app.data.work_experiences import work_experiences
 from app.data.education import educations
 from app.data.hobbies import hobbies
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 load_dotenv()
 app = Flask(__name__)
+mydb=MySQLDatabase(os.getenv('MYSQL_DATABASE'),user=os.getenv('MYSQL_USER'), password=os.getenv('MYSQL_PASSWORD'), host=os.getenv('MYSQL_HOST'), port=3306)
+
+print(mydb)
 
 @app.route("/")
 def index():
     return render_template("index.html",
                            title="Smriti Rangaragajan",
                            url=os.getenv("URL"))
+
+class TimelinePost(Model):
+    name=CharField()
+    email=CharField()
+    content=TextField()
+    created_at=DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 # ---------- Work ----------
 @app.route("/work")
@@ -42,3 +65,37 @@ def hobbies_page():
 def map_page():
     return render_template("map.html",
                            title="Countries I've Visited")
+
+@app.route('/test-post', methods=['POST'])
+def test_post():
+    print("🔥 TEST POST received")
+    return {"status": "ok"}
+
+# ---------- Timeline Post ----------
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    print("✅ Flask received a POST request!")  # 👈 Add this
+    name=request.form['name']
+    email=request.form['email']
+    content=request.form['content']
+    timeline_post=TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts':[
+            model_to_dict(p) for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+@app.route('/api/timeline_post/<int:id>', methods=['DELETE'])
+def delete_timeline_post(id):
+    try:
+        post = TimelinePost.get_by_id(id)
+        post.delete_instance()
+        return {"deleted": id}
+    except TimelinePost.DoesNotExist:
+        return {"error": f"Post with ID {id} does not exist"}, 404
+
