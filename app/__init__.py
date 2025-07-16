9cc3d9a455e228ebd17459ebb24be9088197d04a
Nada Feteiha <nada.feteiha@gmail.com>
2025-07-16 14:38:33 -0700

    add req and some test cases

--- app/__init__.py
+++ app/__init__.py
@@ -17,8 +17,18 @@ pymysql.install_as_MySQLdb()
 
 load_dotenv()
 app = Flask(__name__)
-mydb=MySQLDatabase(os.getenv('MYSQL_DATABASE'),user=os.getenv('MYSQL_USER'), password=os.getenv('MYSQL_PASSWORD'), host=os.getenv('MYSQL_HOST'), port=3306)
 
+
+if os.getenv("TESTING") == "true":
+    print("Running in test mode...")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv('MYSQL_DATABASE'),
+                         user=os.getenv('MYSQL_USER'),
+                         password=os.getenv('MYSQL_PASSWORD'),
+                         host=os.getenv('MYSQL_HOST'),
+                         port=3306)
+    
 print(mydb)
 
 @app.route("/")
